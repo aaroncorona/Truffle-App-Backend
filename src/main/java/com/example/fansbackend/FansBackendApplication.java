f97bb85910e3e6faf6f130198d33e62330972765
= <amc1140@aol.com>
2022-05-17 10:43:06 -0700

    Creating WAR file to deploy to AWS EBS Tomcat

--- src/main/java/com/example/fansbackend/FansBackendApplication.java
+++ src/main/java/com/example/fansbackend/FansBackendApplication.java
@@ -3,10 +3,10 @@ package com.example.fansbackend;
 import com.example.fansbackend.Repository.FanRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FansBackendApplication {
-
+public class FansBackendApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(FansBackendApplication.class, args);
 	}
