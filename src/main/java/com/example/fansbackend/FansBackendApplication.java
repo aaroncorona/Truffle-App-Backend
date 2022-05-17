db003f98a44498998c24495e886b629e9428d2f4
= <amc1140@aol.com>
2022-05-17 15:18:28 -0700

    Updating WAR file to align with the latest AWS RDS Config

--- src/main/java/com/example/fansbackend/FansBackendApplication.java
+++ src/main/java/com/example/fansbackend/FansBackendApplication.java
@@ -1,14 +1,19 @@
 package com.example.fansbackend;
 
+import com.example.fansbackend.Controller.FanController;
 import com.example.fansbackend.Repository.FanRepository;
+import com.example.fansbackend.Service.FanService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FansBackendApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(FansBackendApplication.class, args);
+
 	}
 
 }
