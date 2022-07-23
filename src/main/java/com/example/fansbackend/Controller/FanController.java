6827247a59007ba3cab9c7952e0c336710fc8e5b
= <amc1140@aol.com>
2022-07-22 17:02:23 -0700

    Adding a better toggle ability between application configs for running the APIs on AWS vs a Localhost

--- src/main/java/com/example/fansbackend/Controller/FanController.java
+++ src/main/java/com/example/fansbackend/Controller/FanController.java
@@ -5,7 +5,6 @@ import com.example.fansbackend.Service.FanService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.List;
